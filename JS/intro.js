1cc3b666fa46fbb260f32c18bc54c636c0915098
unknown <vutienhr@gmail.com>
2024-11-02 12:11:53 +0700

    Push lan 1

---
+++ JS/intro.js
@@ -0,0 +1,6 @@
+alert("Helllo");
+var name =parseInt(prompt(' Nhập tuổi  bạn' ));
+let class1 ='23ai';
+let age=18;
+alert('Sinh vien '+ name + ' hoc lop ' + class1 + ', nam nay ' + age);
+alert(`Tuoi nam nay la ${age}`); 
