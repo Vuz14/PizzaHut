9d22d794b9ad3990d16b4dc3908ca1e9eed5f5d0
kemkem100405 <ntpthao100405@gmail.com>
2024-11-07 22:32:32 +0700

    Update

--- Web Pizaa/assets/js/contact-form-script.js
+++ Web Pizaa/assets/js/contact-form-script.js
@@ -1,9 +1,6 @@
-/*==============================================================*/
-// Klev Contact Form  JS
-/*==============================================================*/
-(function ($) {
+(function($) {
     "use strict"; // Start of use strict
-    $("#contactForm").validator().on("submit", function (event) {
+    $("#contactForm").validator().on("submit", function(event) {
         if (event.isDefaultPrevented()) {
             // handle the invalid form...
             formError();
@@ -16,7 +13,7 @@
     });
 
 
-    function submitForm(){
+    function submitForm() {
         // Initiate Variables With Form Content
         var name = $("#name").val();
         var email = $("#email").val();
@@ -30,34 +27,34 @@
             type: "POST",
             url: "assets/php/form-process.php",
             data: "name=" + name + "&email=" + email + "&phone_number=" + phone_number + "&subject=" + subject + "&website=" + website + "&message=" + message,
-            success : function(text){
-                if (text == "success"){
+            success: function(text) {
+                if (text == "success") {
                     formSuccess();
                 } else {
                     formError();
-                    submitMSG(false,text);
+                    submitMSG(false, text);
                 }
             }
         });
     }
 
-    function formSuccess(){
+    function formSuccess() {
         $("#contactForm")[0].reset();
         submitMSG(true, "Message Submitted!")
     }
 
-    function formError(){
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    function formError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
             $(this).removeClass();
         });
     }
 
-    function submitMSG(valid, msg){
-        if(valid){
+    function submitMSG(valid, msg) {
+        if (valid) {
             var msgClasses = "h4 text-left tada animated text-success";
         } else {
             var msgClasses = "h4 text-left text-danger";
         }
         $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-}(jQuery)); // End of use strict
+}(jQuery));
